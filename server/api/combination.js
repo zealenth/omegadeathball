a3a50471dba9d88f11ce4c49845c44c9d6cccff2
dtcs400 <dtcs400@gmail.com>
2015-04-12 15:32:01 -0400

    Fixed combination failing when passing array of one

--- server/api/combination.js
+++ server/api/combination.js
@@ -14,16 +14,17 @@
 exports.combination = function(entry){
 	var rv = [];
 	var n = entry.length - 1;
-
-	while(n >= 0){
-		var currentCombination = [];
-		for(var i = 0; i < entry.length; i++){
-			if(i != n){
-				currentCombination.push(entry[i]);
+	if(n > 0){
+		while(n >= 0){
+			var currentCombination = [];
+			for(var i = 0; i < entry.length; i++){
+				if(i != n){
+					currentCombination.push(entry[i]);
+				}
 			}
+			rv.push(currentCombination);
+			n--;
 		}
-		rv.push(currentCombination);
-		n--;
-	}
+	}	
 	return rv;
 }
