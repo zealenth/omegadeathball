0c5dd5e08cc0579476a93cd439daa7436cad7ae9
andSpace <andSpace@users.noreply.github.com>
2015-04-18 02:26:39 -0500

    So not every VPS has 4GB of ram... go figure

---
+++ server/chew.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir "tmp";
+`split -d -l 100000 $1`
+mv x* tmp;
+
+for file in ./tmp/*
+do
+  rm tmp.js
+  `head -n 1 $1 >> tmp.js`
+  cat $file >> tmp.js
+  `tail -n 1 $1 >> tmp.js`
+  mongo localhost:27017/urf-dev tmp.js
+   echo $file
+done
+
+rm tmp/*
